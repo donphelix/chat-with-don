86084b71d97480e07f3a2b0235503270ee2e3263
donphelix <margiomondi@gmail.com>
2022-07-19 14:22:18 +0300

    Fixing The prop `xs` of `Grid` must be used on `item` error

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -9,17 +9,12 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import Fab from '@material-ui/core/Fab';
-import SendIcon from '@material-ui/icons/Send';
 import {Chip} from "@material-ui/core";
 
 const useStyles = makeStyles({
@@ -85,8 +80,8 @@ const Chat = () => {
                     <Grid item xs={11} style={{backgroundColor: 'white', borderRadius: '10px'}}>
                         <TextField placeholder="Start Typing" InputProps={{disableUnderline: true}} fullWidth/>
                     </Grid>
-                    <Grid xs={1} align="right">
-                        <Chip clickable label="Send" color="success"
+                    <Grid item xs={1} align="right">
+                        <Chip clickable onClick={() => console.log("Coming soon")} label="Send"
                               style={{backgroundColor: 'white', padding: '4px'}}/>
                     </Grid>
                 </Grid>
